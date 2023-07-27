a2a6069113ad51f4f6ef62a280cb934baaf376ff
MaddiletiMurali <muralidharreddy2322@gmail.com>
2023-07-27 17:34:49 +0530

    first commit

---
+++ src/main/java/com/mic/sboot/service/MetadataStorage.java
@@ -0,0 +1,29 @@
+package com.mic.sboot.service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MetadataStorage {
+
+	public static boolean storeMetadata(String createTableStatement) {
+		
+		// Implement storing the metadata in metadata.txt
+		boolean flag = false;
+		try {
+			// FileOutputStream writer = new FileOutputStream("metadata.txt", true);
+			// writer.write(createTableStatement);
+			// writer.newLine();
+			PrintWriter out = new PrintWriter(new FileOutputStream(new File("metadata.txt")), false);
+			out.println(createTableStatement);
+			out.close();
+			flag = true;
+			System.out.println("Hiiiii");
+
+		} catch (IOException e) {
+			throw new RuntimeException("Error while storing metadata.", e);
+		}
+		return flag;
+	}
+}
